9a54884a0999606412591b5a7bf3b42ce1c5fcdd
= <=>
2021-11-05 01:33:47 +0800

    11-5 2nd Push

--- lib/views/admin/itemview.dart
+++ lib/views/admin/itemview.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:itelectivesix/models/items.dart';
 import 'package:itelectivesix/constants.dart';
+
 class ItemView extends StatelessWidget {
   final Item item;
 
@@ -11,53 +12,55 @@ class ItemView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: InkWell(
-          child: Icon(Icons.home, color: gradPurple, size: 56),
-          // onTap: (){
-          // },
+        title: Row(
+          children: const [
+            Icon(Icons.home, color: gradPurple, size: 56),
+            Text(
+              "Housy Appliances",
+              style: TextStyle(color: Colors.black),
+            ),
+          ],
         ),
-        backgroundColor: Colors.grey[300],
+        backgroundColor: Colors.white,
       ),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-            flex: 3,
-            child: Container (
-              margin: EdgeInsets.only(
-              top: 50,
-              left: 70,
-              right: 70,
-              bottom: 50
-              ),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [gradPurple, gradPink, gradYellow],
+        )),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+                flex: 3,
+                child: Container(
+                    margin: EdgeInsets.only(
+                        top: 50, left: 70, right: 70, bottom: 50),
+                    child: Container(
+                      height: 600,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.white),
+                        borderRadius:
+                            const BorderRadius.all(const Radius.circular(10)),
+                        color: Colors.white,
+                        image: DecorationImage(
+                            image: NetworkImage(item.photoUrl),
+                            fit: BoxFit.fill),
+                      ),
+                    ))),
+            Expanded(
+              flex: 4,
               child: Container(
-              height: 600,
-              decoration: BoxDecoration(
-                border: Border.all(width: 1, color: Colors.white),
-                borderRadius: const BorderRadius.all(const Radius.circular(10)),
-                color: Colors.white,
-                image: DecorationImage(
-                  image: NetworkImage(item.photoUrl),
-                  fit: BoxFit.fill
+                margin:
+                    EdgeInsets.only(top: 50, left: 70, right: 70, bottom: 50),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.white),
+                  borderRadius:
+                      const BorderRadius.all(const Radius.circular(10)),
+                  color: Colors.white,
                 ),
-              ),
-            )
-            )
-          ),
-          Expanded(
-            flex: 4,
-            child: Container(
-              margin: EdgeInsets.only(
-                top: 50,
-                left: 70,
-                right: 70,
-                bottom: 50
-              ),
-              decoration: BoxDecoration(
-              border: Border.all(width: 1, color: Colors.white),
-              borderRadius: const BorderRadius.all(const Radius.circular(10)),
-              color: Colors.white,
-              ),
-              child: Column(
+                child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
@@ -70,13 +73,16 @@ class ItemView extends StatelessWidget {
                       child: Text(
                         item.name,
                         textAlign: TextAlign.center,
-                          style: const TextStyle(
+                        style: const TextStyle(
                           color: Colors.black,
                           fontSize: 30,
                           fontWeight: FontWeight.w300,
                           fontFamily: 'FuturaBold',
                         ),
                       ),
+                    ),
+                    const SizedBox(
+                      height: 20,
                     ),
                     Container(
                       margin: EdgeInsets.only(
@@ -88,7 +94,7 @@ class ItemView extends StatelessWidget {
                       child: Text(
                         "₱ " + item.price.toString(),
                         textAlign: TextAlign.center,
-                          style: const TextStyle(
+                        style: const TextStyle(
                           color: Colors.black,
                           fontSize: 15,
                           fontWeight: FontWeight.w300,
@@ -96,17 +102,19 @@ class ItemView extends StatelessWidget {
                         ),
                       ),
                     ),
+                    const SizedBox(
+                      height: 20,
+                    ),
                     Container(
                       margin: EdgeInsets.only(
-                        // top: 20,
-                        left: 30,
-                        right: 30,
-                        bottom: 50
-                      ),
+                          // top: 20,
+                          left: 30,
+                          right: 30,
+                          bottom: 50),
                       child: Text(
                         item.description,
                         textAlign: TextAlign.justify,
-                          style: const TextStyle(
+                        style: const TextStyle(
                           color: Colors.black,
                           fontSize: 16,
                           height: 1.5,
@@ -115,75 +123,38 @@ class ItemView extends StatelessWidget {
                         ),
                       ),
                     ),
-                    Center(
-                      child: Container(
-                      margin: EdgeInsets.only(top: 20),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.black54,
-                          onPrimary: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                        ),
-                        child: Container(
-                            width: 400,
-                            height: 50,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                const Text(
-                                  "Add to Bag",
-                                  style: const TextStyle(
-                                  // color: Colors.white,
-                                  fontSize: 16,
-                                  // height: 1.5,
-                                  fontWeight: FontWeight.w300,
-                                  fontFamily: 'FuturaLight',
-                                ),
-                               ),
-                              ]
-                            ),
-                          ),
-                        ),
-                      ),
+                    const SizedBox(
+                      height: 20,
                     ),
                     Center(
                       child: Container(
-                      margin: EdgeInsets.only(top: 10),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.red[400],
-                          onPrimary: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15),
+                        margin: EdgeInsets.only(top: 20),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            primary: gradPurple,
+                            onPrimary: Colors.white,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
                           ),
-                        ),
-                        child: Container(
+                          child: Container(
                             width: 400,
                             height: 50,
                             child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                const Text(
-                                  "Favorite  ",
-                                  style: const TextStyle(
-                                  // color: Colors.white,
-                                  fontSize: 16,
-                                  // height: 1.5,
-                                  fontWeight: FontWeight.w300,
-                                  fontFamily: 'FuturaLight',
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Text(
+                                    "Add to Cart",
+                                    style: const TextStyle(
+                                      // color: Colors.white,
+                                      // height: 1.5,
+                                      fontWeight: FontWeight.w300,
+                                      fontFamily: 'FuturaLight',
+                                    ),
                                   ),
-                                ),
-                                const FaIcon(
-                                  FontAwesomeIcons.heart,
-                                )
-                              ]
-                            ),
+                                ]),
                           ),
                         ),
                       ),
@@ -194,6 +165,7 @@ class ItemView extends StatelessWidget {
             ),
           ],
         ),
+      ),
       // RIGHT PORTION DRAWER
       // body: Center(
       //   child: Text(
